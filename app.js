4311f4ad227d9b85223e6fc5c9a70cd874142687
KUCHARLAPATI VIJAYA KUMAR VARMA <vijayakumarkucharlapati@gmail.com>
2023-12-28 17:26:59 +0530

    teacher page completed some changes remaining in the reports page

--- app.js
+++ app.js
@@ -11,24 +11,29 @@ app.get("/",(req,res)=>{
  })
 app.get("/studentResultPage",(req,res)=>{
     res.render("studentResultPage");
- })
+})
 app.get("/TeacherPage",(req,res)=>{
     res.render("TeacherPage");
- })
- app.get("/add",(req,res)=>{
+})
+app.get("/add",(req,res)=>{
     res.render("add");
- })
- app.get("/search",(req,res)=>{
+})
+app.get("/search",(req,res)=>{
     res.render("search");
- })
- app.get("/update",(req,res)=>{
+})
+app.get("/update",(req,res)=>{
     res.render("update");
- })
- app.get("/delete",(req,res)=>{
+})
+app.get("/delete",(req,res)=>{
     res.render("delete");
- })
-
-
+})
+// app.get("/Report",(req,res)=>{
+//     res.render('Report');
+// })
+app.get("/AllotMarks",(req,res)=>{
+    res.render('AllotMarks')
+})
+ 
 app.post("/student-submit",(req,res)=>{
     const username = req.body.username;
    const password = req.body.password; 
@@ -80,6 +85,7 @@ app.post("/teacher-submit",(req,res)=>{
         }
     })
 })
+
 app.get("/addstudent", (req, res) => {
         const { name, rollno, password, year, branch } = req.query;
         let qry = 'INSERT INTO student (Name, RollNumber, Password, Year, Branch) VALUES (?, ?, ?, ?, ?)';
@@ -87,11 +93,100 @@ app.get("/addstudent", (req, res) => {
           if (err) {
             throw err;
           } else {
-            res.render('add', { addmsg: true });
+            res.render('add',{addmsg:true});
           }
         });
 });
       
+app.get("/removestudent",(req,res)=>{
+    const {rollno} = req.query;
+    let qry = 'delete from student where RollNumber = ?';
+    mysql.query(qry,[rollno],(err,results)=>{
+        if(err){
+            throw err;
+        }
+        else{
+            res.render('TeacherPage',{dmsg1:true});
+        }
+    })
+})
+
+app.get("/searchstudent",(req,res)=>{
+    const {rollno} = req.query;
+    let qry = "select * from student where RollNumber=?";
+    mysql.query(qry,[rollno],(err,results)=>{
+       if(err){
+          throw err;
+       }
+       else{
+          if(results.length>0){
+             res.render("search",{msg1:true})
+          }
+          else{
+             res.render("search",{msg2:true})
+          }
+       }
+    })
+ })
+
+ app.get("/updatesearch",(req,res)=>{
+    const {rollno} = req.query;
+    let qry = "select * from student where RollNumber=?";
+    mysql.query(qry,[rollno],(err,results)=>{
+       if(err){
+          throw err; 
+       }
+       else{
+          if(results.length>0){
+             res.render("update",{msg1:true,data:results})
+          }
+          else{
+             res.render("update",{msg2:true})
+          }
+       }
+    })
+ })
+
+
+ app.get("/updatestudent",(req,res)=>{
+    const {name,rollno} = req.query;
+    let qry = "update student set Name=?,Queries=NULL where RollNumber = ?";
+    mysql.query(qry,[name,rollno],(err,results)=>{
+       if(err){
+          throw err;
+       }
+       else {
+          if(results.affectedRows>0){
+             res.render("update",{umsg:true});
+          }
+       }
+    })
+ });
+
+app.get('/view',(req,res)=>{
+    let qry = 'select * from student order by RollNumber asc';
+    mysql.query(qry,(err,results)=>{
+        if(err){
+            throw err;
+        }
+        else{
+            res.render('view',{data:results});
+        }
+    })
+})
+
+app.get("/Report",(req,res)=>{
+    let qry = 'select * from student where Queries IS NOT NULL';
+    mysql.query(qry,(err,results)=>{
+        if(err){
+            throw err;
+        }
+        else{
+            res.render('Report',{data:results,noOfQueries:results.length})
+        }
+    })
+})
+
 app.listen(port,(err)=>{
     if(err){
         throw err;
